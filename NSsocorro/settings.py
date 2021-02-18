a0f39a4e530a65473155bab67134f4e19e2f6a2a
AndresYepesM <aymoreno567@gmail.com>
2021-02-17 19:23:43 -0500

    Password test 3

--- NSsocorro/settings.py
+++ NSsocorro/settings.py
@@ -145,10 +145,10 @@ LOGOUT_REDIRECT_URL = 'home'
 # Email Configurations
 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'djangopruebaemail59@gmail.com'
-EMAIL_HOST_PASSWORD = 'Django26369180'
+EMAIL_HOST_USER = 'bb731b2f948c506d'
+EMAIL_HOST_PASSWORD = 'B2gSkXF2hCQJefWu6ag1HQf2'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.cloudmta.net'
 EMAIL_PORT = 587
 
 # CKEDITOR.
