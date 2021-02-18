fd90829ffbcf7b9782b36df6991e09f3a8b2794d
AndresYepesM <aymoreno567@gmail.com>
2021-02-18 12:46:34 -0500

    debug2

--- NSsocorro/settings.py
+++ NSsocorro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nig5q!xw^gdqk_1updoz+@59jt!tdz+sqhfe6!sq)(@_&0it%('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['web-page-test.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ['192.168.1.72']
 
 
