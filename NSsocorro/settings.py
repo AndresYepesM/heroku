3954fbd0ad634c2b8d04155d7ecfe69889fa2c9a
AndresYepesM <aymoreno567@gmail.com>
2021-02-18 12:57:23 -0500

    debug2 fixed static files3

--- NSsocorro/settings.py
+++ NSsocorro/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
--- NSsocorro/wsgi.py
+++ NSsocorro/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NSsocorro.settings')
 
 application = get_wsgi_application()
+
+
+from NSsocorro import MyWSGIApp
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root='/static/')
+application.add_files('/static/', prefix='more-files/')
