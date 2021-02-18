5803dd7f72c840f24c55d8e6741af769c318d165
AndresYepesM <aymoreno567@gmail.com>
2021-02-17 19:07:02 -0500

    Password test 2

--- NSsocorro/settings.py
+++ NSsocorro/settings.py
@@ -144,13 +144,12 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # Email Configurations
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'djangopruebaemail59@gmail.com'
 EMAIL_HOST_PASSWORD = 'Django26369180'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 
 # CKEDITOR.
 CKEDITOR_UPLOAD_PATH = "upload/"
